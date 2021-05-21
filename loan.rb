da647a9baca1b3408d3bfc29f027a1975369980d
Shubham <shubhs.8905@gmail.com>
2021-05-21 12:34:31 +0530

    Method will_get_loan modified in loan.rb

--- loan.rb
+++ loan.rb
@@ -1,7 +1,7 @@
 module Loan
     # country will get loan from INF if gdp>=5 and state is Developed
     # country will get loan from WB if gdp>=4 and state is developed or developing
-    def will_get_loan(gdp, state)
+    def print_loan_type(gdp, state)
         if (gdp >= 5 && state == "Developed") && (gdp >= 3 && state == "Developed" || state == "Developing")
             puts "Country will get loan from INF as well as WB"
         elsif (gdp >= 5 && state == "Developed")
