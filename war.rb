5024a64fb24973ee6a23460e992ca12193ae1746
Shubham <shubhs.8905@gmail.com>
2021-05-21 12:32:17 +0530

    New line at EOF added and method can_win_war modified in war.rb

--- war.rb
+++ war.rb
@@ -1,10 +1,6 @@
 module War
     #  A country can win a war if no of states is more than 10, population is more than 20 crores and army strength is more than 10 lakh
-    def can_win_war(country)
-        if country.states.count>=1 && country.population >= 200000000 && country.army_strength >= 1000000
-            puts "Country can win the war "
-        else
-            puts "Country cannot win the war "
-        end
+    def can_win_war?(country)
+        return country.states.count>=1 && country.population >= 200000000 && country.army_strength >= 1000000 
     end
 end
