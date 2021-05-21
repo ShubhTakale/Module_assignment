a88715c137fc0853be997eb1ec659ee12b2b7d98
Shubham <shubhs.8905@gmail.com>
2021-05-21 12:33:46 +0530

    Method can_have_seat_in_UN modified in seat_in_UN.rb

--- seat_in_UN.rb
+++ seat_in_UN.rb
@@ -1,10 +1,6 @@
 module SeatInUN
     # A country can have seat if its army strength is greater than 2 lakh, state is developed 
-    def can_have_seat_in_UN (country)
-        if country.army_strength > 200000 && country.country_state== "Developed"
-            puts "Country can have seat in UN Security Council "
-        else
-            puts "Country cannot have seat in UN Security Council "
-        end
+    def can_have_seat_in_UN?(country)
+        return country.army_strength > 200000 && country.country_state== "Developed"
     end
 end
